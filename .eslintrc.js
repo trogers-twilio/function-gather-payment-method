99adb0d44dbbd087dc5f25b3aed6dcd9572c1186
Terence Rogers <trogers@twilio.com>
2019-03-17 23:24:27 -0700

    Updated eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules: {
+        "no-console": "off",
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+        "object-curly-newline": 0
+    }
+};
